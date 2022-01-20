93174457d7dd8489f7c133b47696046ed6729b18
91143423 <leahleco@MacBook-Pro-LL.local>
2022-01-20 10:22:28 +0100

    infinite scroll message

--- src/view/layout.php
+++ src/view/layout.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://use.typekit.net/ksz3knm.css">
   <?php echo $css; ?>
     <title>the use of Bodoni</title>
 </head>
--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -1,4 +1,11 @@
 <section class="content">
+  <div class="infinite-scroll">
+  <div class="infinite-scroll-1">
+    <div class="infinite-scroll-2">
+      <h1  class="infinite-scroll-text">The use of Bodoni • The use of Bodoni • The use of Bodoni • The use of Bodoni •</h1>
+    </div>
+  </div>
+  </div>
   <div>
       <ul>
           <li>inventor:</li>
