6a7740496c9fcef1cba2c7164b3b9b4d5a04edf6
91143423 <leahleco@MacBook-Pro-LL.local>
2022-01-24 18:33:14 +0100

    updates dist

--- dist/controller/PagesController.php
+++ dist/controller/PagesController.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/Controller.php';
 require_once __DIR__ . '/../model/MainText.php';
 require_once __DIR__ . '/../model/VarSvg.php';
 require_once __DIR__ . '/../model/SeenInImg.php';
-require_once __DIR__ . '/../model/BodoniRule.php';
 
 class PagesController extends Controller {
 
@@ -17,9 +16,6 @@ class PagesController extends Controller {
 
     $seen_in_imgs = SeenInImg::where('path', '!=', 'NULL')->get();
     $this->set('seen_in_imgs',$seen_in_imgs);
-
-    $bodoni_rules = BodoniRule::where('title', '!=', 'NULL')->get();
-    $this->set('bodoni_rules',$bodoni_rules);
 
   }
 }
